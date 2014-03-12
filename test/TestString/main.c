8b18bb41ae69c138cc8e4894ee62a456879f3f26
sleepwalking <2657202503@qq.com>
2014-03-12 09:53:38 +0800

    Convert

--- test/TestString/main.c
+++ test/TestString/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "Structure/String.h"
+#include "Misc/Convert.h"
 
 int main(void)
 {
@@ -9,8 +10,11 @@ int main(void)
     String_SetChars(& a, "asdfsf from a string.");
     String_SetChars(& b, "string2.");
     String_Join(& a, & b);
+    String_SetChars(& c, "234.234324");
+    double x = CDoubleStr(& c);
     Mid(& c, & a, 5, 10);
     printf("%s\n", String_GetChars(& c));
+    printf("%lf, %d\n", x, InStr(& a, & b));
     
     RDelete(& a, & b, & c);
     return 0;
