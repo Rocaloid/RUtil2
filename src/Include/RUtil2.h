70975e798e08e1f46138c1a3896f309c3c396ed1
tuxzz <dorazzsoft@gmail.com>
2014-06-15 18:15:28 +0800

    Base64 Encoder(Buggy)

--- src/Include/RUtil2.h
+++ src/Include/RUtil2.h
@@ -3,11 +3,15 @@
 #endif //__RUtil2_Install
 
 #include <RUtil2/Core/RAlloc.h>
+
 #include <RUtil2/Structure/Array.h>
 #include <RUtil2/Structure/String.h>
 #include <RUtil2/Structure/PMatch.h>
+
 #include <RUtil2/Misc/Convert.h>
 #include <RUtil2/Misc/Sort.h>
+#include <RUtil2/Misc/Base64.h>
+
 #include <RUtil2/IO/File.h>
 #include <RUtil2/IO/WaveFile.h>
 #include <RUtil2/Core/OO.h>
