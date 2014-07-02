0f04be24dfd94b0da816ee3e6139d8a3cabb1104
TuxZZ <dorazzsoft@gmail.com>
2014-07-02 10:06:57 +0800

    Add Module: Tune

--- src/Misc/Tune.h
+++ src/Misc/Tune.h
@@ -1,18 +1,38 @@
-/*
 #ifndef RUTIL2_TUNE_H
 #define RUTIL2_TUNE_H
 
-#include "../Core/OO.h"
 #include "../Structure/String.h"
+#include "../Core/OO.h"
 
-// Return PitchNum.
-// If an error occurrs, -10086 is returned.
-int Tune_SPNToPitch(String* Sorc);
+enum Tune_PitchNameMode
+{
+    AlwaysRising = 0,
+    AlwaysFalling,
+    Mixing,
+    __PADDING__ = INT_MAX
+};
 
-// Return PitchNum.
-// If an error occurrs, -10086 is returned.
-float Tune_FreqToPitch(float Freq);
+void Tune_SetPitchNameMode(enum Tune_PitchNameMode m);
+
+#if 0
+#include "_Tune.h"
+#endif
+#ifdef __RUtil2_Install
+#define _RTAddress "RUtil2/Misc/_Tune.h"
+#else
+#define _RTAddress "Misc/_Tune.h"
+#endif
+
+#define _ClassName
+#define _Attr 1
+
+#define _T1 Float
+#include "../Core/RTemplate.h"
+
+#define _T1 Double
+#include "../Core/RTemplate.h"
+
+#define _T1 Int
+#include "../Core/RTemplate.h"
 
 #endif //RUTIL2_TUNE_H
-*/
-
