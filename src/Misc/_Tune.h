c3255e84584b296a7ab4afa355004f5a709d4e8a
TuxZZ <dorazzsoft@gmail.com>
2014-07-21 18:01:50 +0800

    Add cent tools

--- src/Misc/_Tune.h
+++ src/Misc/_Tune.h
@@ -9,6 +9,9 @@ RTFunc(_T1, Tune_SPNToPitch, String* SPN);
 RTFunc(_T1, Tune_FreqToPitch, _T1 Freq);
 RTFunc(_T1, Tune_PitchToFreq, _T1 Pitch);
 
+RTFunc(_T1, Tune_AddCentToFreq, _T1 Freq, _T1 Cent);
+RTFunc(_T1, Tune_CalcCentFromFreq, _T1 Freq1, _T1 Freq2);
+
 // Time unit is sec.
 RTFunc(_T1, Tune_BeatToTime, _T1 BPM, _T1 BeatCnt);
 RTFunc(_T1, Tune_TimeToBeat, _T1 BPM, _T1 Time);
